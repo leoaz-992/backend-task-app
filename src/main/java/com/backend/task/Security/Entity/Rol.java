f5a9b6ae16505dabfc67af8cb4ac4b538ea4b34a
Leonardo Alvarez <leo92alvarez25@gmail.com>
2023-07-25 23:31:41 -0300

    create entity user and rol

---
+++ src/main/java/com/backend/task/Security/Entity/Rol.java
@@ -0,0 +1,29 @@
+package com.backend.task.Security.Entity;
+
+import com.backend.task.Security.Enums.RolNombre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="roles")
+public class Rol {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private long id;
+    @Enumerated(EnumType.STRING)
+    private RolNombre name;
+    
+}
---
+++ src/main/java/com/backend/task/Security/Enums/RolNombre.java
@@ -0,0 +1,7 @@
+package com.backend.task.Security.Enums;
+
+public enum RolNombre {
+    ROLE_ADMIN,
+    ROLE_USER,
+    
+}
